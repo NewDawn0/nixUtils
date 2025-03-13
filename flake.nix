431eb0c34ba00fd8cd5541925d792bb44c5f7022
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:44:56 +0100

    add new systems options

--- flake.nix
+++ flake.nix
@@ -8,16 +8,11 @@
 
   outputs = { nixpkgs, nix-systems, ... }:
     let
-      eachSystem = args: f:
-        let
-          mkNixpkgs = args.nixpkgs or nixpkgs;
-          mkPkgs = if builtins.hasAttr "mkPkgs" args
-          && builtins.isFunction (args.mkPkgs) then
-            args.mkPkgs
-          else
-            system: mkNixpkgs.legacyPackages.${system};
-        in nixpkgs.lib.genAttrs (import nix-systems)
-        (system: let pkgs = mkPkgs system; in f pkgs);
+      mkDefaultPkgs = system: nixpkgs.legacyPackages.${system};
+      eachSystem =
+        { nixpkgs ? nixpkgs, systems ? nix-systems, mkPkgs ? mkDefaultPkgs }:
+        f:
+        nixpkgs.lib.genAttrs systems (system: let pkgs = mkPkgs; in f pkgs);
       basic = {
         path = ./templates/basic;
         description = "nix flake init -t nixUtils#basic";
